2d330b83a032b5a8a154238815b50d13dd168010
Luis Fernando <luis.silva9902@gmail.com>
2022-06-12 20:12:15 -0300

    Add styles in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
+import styles from './styles.module.scss'
+
 export function Header() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <header className={styles.container}>
+      <img src="/images/logo.png" alt="Logo" />
+      <h1>spacetraveling</h1>
+      <span>.</span>
+    </header>
   )
 }
